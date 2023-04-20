6803d44dba19ede62fde46e65b9b56f604bbc567
Domai <tim.barsch@rub.de>
2023-04-20 12:50:21 +0200

    Player switching
    - Some errors occur but it works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,11 @@ public class Player : MonoBehaviour
     // Speed of the character
     [SerializeField] float speed = 3f;
     // speed modificators, toggle
-    int[] speedMod = new int [] {1, 2, 4};
+    int[] speedMod = new int[] {1, 2, 4};
     int indexSpeedMod = 0;
     GameObject player;
-    // Start is called before the first frame update
 
+    // Start is called before the first frame update
     void Start()
     {
         // from https://gamedev.stackexchange.com/questions/171565/accesing-child-gameobjects-of-gameobject
@@ -25,7 +25,7 @@ public class Player : MonoBehaviour
         // index 0 ist the camera
         this.transform.GetChild(0).gameObject.SetActive(true);
         
-
+        // Second child is first character and so one
         player = this.transform.GetChild(1).gameObject;
         player.SetActive(true);
     }
@@ -44,14 +44,30 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S)) moveVector.y = -1;
         if (Input.GetKey(KeyCode.D)) moveVector.x = 1;
         
-        if (Input.GetKeyDown(KeyCode.F))
-        {
+        // change character component colors by pressing F
+        if (Input.GetKeyDown(KeyCode.F)) {
             SpriteRenderer [] childSpriteRenderer = player.GetComponentsInChildren<SpriteRenderer>();
             foreach(SpriteRenderer sr in childSpriteRenderer) {
                 Color color = Random.ColorHSV();
                 sr.color = color;
             }
         }
+
+        // change character by pressing 1,2,3 ...
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            changePlayer(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            changePlayer(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            changePlayer(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            changePlayer(4);
+        }
+        
+        
         // taken from exercise
         // Normalize vector, so that magnitude for diagonal movement is also 1
         moveVector.Normalize();
@@ -59,5 +75,11 @@ public class Player : MonoBehaviour
         moveVector = moveVector * speedMod[indexSpeedMod];
          // Frame rate independent movement
         transform.position += Time.deltaTime * speed * moveVector;
+    }
+
+    private void changePlayer(int layer) {
+        player.SetActive(false);
+        player = this.transform.GetChild(layer).gameObject;
+        player.SetActive(true);
     }
 }
